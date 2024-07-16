12f473b61b80dce676a8913edecc5286d031c0d2
Prakarsh Singh <108565574+PrakarshSingh5@users.noreply.github.com>
2024-07-16 20:15:47 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import {BsArrowUpRight, BsChevronDown} from "react-icons/bs"
 import Typerwriter from "typewriter-effect";
 import { FaFileDownload } from "react-icons/fa";
-import photo from '../assets/imgprak.png';
+import photo from '../assets/1670067803439.jpg';
 
 const Home = () => {
     const animations={
